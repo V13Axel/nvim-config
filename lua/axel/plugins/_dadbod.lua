0f43fee51693227a3851241ee67a557adeaf38ca
V13Axel <alex@stechstudio.com>
2025-07-17 10:47:51 -0400

    Move dadbod keymap to dadbod plugin import

--- lua/axel/plugins/_dadbod.lua
+++ lua/axel/plugins/_dadbod.lua
@@ -1,5 +1,18 @@
 return {
-    'tpope/vim-dadbod',
-    'kristijanhusak/vim-dadbod-completion',
     'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+        { 'tpope/vim-dadbod',                     lazy = true },
+        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    init = function()
+        vim.keymap.set(
+            'n',
+            '<M-D>',
+            function()
+                vim.api.nvim_command('tab DBUI')
+            end,
+            { silent = true }
+        )
+        vim.g.db_ui_use_nerd_fonts = 1
+    end
 }
