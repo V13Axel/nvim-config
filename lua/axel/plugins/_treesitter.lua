7468d1554ab77e033075a041b3dbe8e445e544d2
V13Axel <alex@stechstudio.com>
2026-03-02 12:33:53 -0500

    Finally migrate nvim-treesitter to new `main`

--- lua/axel/plugins/_treesitter.lua
+++ lua/axel/plugins/_treesitter.lua
@@ -1,17 +1,27 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
+    lazy = false,
     branch = 'main',
+    build = ':TSUpdate',
     dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
+        { 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'main' },
         'nvim-treesitter/nvim-treesitter-context'
     },
-    opts = {
-        ensure_installed = {
+    config = function()
+        -- blade is now a tier-2 parser in nvim-treesitter main; no custom registration needed.
+        vim.filetype.add({
+            pattern = {
+                ['.*%.blade%.php'] = 'blade',
+            }
+        })
+
+        -- Install parsers (no-op if already installed)
+        require('nvim-treesitter').install({
             'comment',
             'css',
             'diff',
             'dockerfile',
+            'dtd',      -- required by xml
             'git_config',
             'html',
             'blade',
@@ -22,127 +32,108 @@ return {
             'php_only',
             'kdl',
             'rust',
+            'sql',
             'vim',
             'regex',
             'bash',
+            'markdown',
             'markdown_inline',
-        },
-        auto_install = true,
-        sync_install = true,
-        highlight = {
-            enable = true,
-
-            -- NOTE: This will slow things down, if you need to re-enable it, make sure you know why.
-            -- Disabled on 2024-03-13, because it's slowing down the editor.
-            additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-        textobjects = {
-            lsp_interop = {
-                enable = true,
-                border = 'none',
-                floating_preview_opts = {},
-                peek_definition_code = {
-                    ["<leader>df"] = "@function.outer",
-                    ["<leader>dF"] = "@class.outer",
-                },
-            },
-            swap = {
-                enable = true,
-                swap_next = {
-                    ['<leader>na'] = '@parameter.inner',
-                    ['<leader>nf'] = '@function.outer',
-                },
-                swap_previous = {
-                    ['<leader>pa'] = '@parameter.inner',
-                    ['<leader>pf'] = '@function.outer',
-                }
-            },
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ['if'] = '@function.inner',
-                    ['af'] = '@function.outer',
-                    ['ia'] = '@parameter.inner',
-                    ['aa'] = '@parameter.outer',
-                    ['ac'] = '@class.outer',
-                    ['ic'] = '@class.inner',
-                    ['a/'] = '@comment.outer',
-                    ['i/'] = '@comment.inner',
-                    ['ii'] = '@conditional.inner',
-                    ['ai'] = '@conditional.outer',
-                    ['ix'] = '@attribute.inner',
-                    ['ax'] = '@attribute.outer',
-                },
-            },
-            move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-                    [']f'] = '@function.outer',
-                    [']]'] = '@class.outer',
-                    [']a'] = '@parameter.inner',
-                    [']i'] = '@conditional.outer',
-                    [']/'] = '@comment.outer',
-                },
-                goto_next_end = {
-                    [']F'] = '@function.outer',
-                    [']['] = '@class.outer',
-                    [']A'] = '@parameter.inner',
-                    [']I'] = '@conditional.outer',
-                    [']?'] = '@comment.outer',
-                },
-                goto_previous_start = {
-                    ['[f'] = '@function.outer',
-                    ['[['] = '@class.outer',
-                    ['[a'] = '@parameter.inner',
-                    ['[i'] = '@conditional.outer',
-                    ['[/'] = '@comment.outer',
-                },
-                goto_previous_end = {
-                    ['[F'] = '@function.outer',
-                    ['[]'] = '@class.outer',
-                    ['[A'] = '@parameter.inner',
-                    ['[I'] = '@conditional.outer',
-                    ['[?'] = '@comment.outer',
-                },
-            },
-        }
-    },
-    config = function(_, opts)
-        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        parser_config.blade = {
-            install_info = {
-                url = "https://github.com/EmranMR/tree-sitter-blade",
-                files = { "src/parser.c" },
-                branch = "main",
-                generate_requires_npm = true,
-                requires_generate_from_grammar = true,
-            },
-            filetype = "blade"
-        }
-
-        vim.filetype.add({
-            pattern = {
-                ['.*%.blade%.php'] = 'blade',
-            }
+            'xml',
         })
 
+        -- Enable highlighting and indentation per filetype
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = {
+                'css', 'diff', 'dockerfile', 'git_config',
+                'html', 'blade', 'javascript', 'json', 'lua',
+                'php', 'php_only', 'kdl', 'rust', 'sql', 'vim',
+                'regex', 'bash', 'markdown', 'markdown_inline',
+                'xml', 'dtd',
+            },
+            callback = function()
+                vim.treesitter.start()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+
+        -- treesitter-context
         require('treesitter-context').setup()
-        require('nvim-treesitter.configs').setup(opts)
-        local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+        -- textobjects: configure select and move behaviour
+        require("nvim-treesitter-textobjects").setup {
+            select = {
+                lookahead = true,
+            },
+            move = {
+                set_jumps = true,
+            },
+        }
+
+        -- textobjects: select
+        local select = require("nvim-treesitter-textobjects.select")
+        local sel = function(q) return function() select.select_textobject(q, "textobjects") end end
+        vim.keymap.set({ "x", "o" }, "if", sel("@function.inner"))
+        vim.keymap.set({ "x", "o" }, "af", sel("@function.outer"))
+        vim.keymap.set({ "x", "o" }, "ia", sel("@parameter.inner"))
+        vim.keymap.set({ "x", "o" }, "aa", sel("@parameter.outer"))
+        vim.keymap.set({ "x", "o" }, "ac", sel("@class.outer"))
+        vim.keymap.set({ "x", "o" }, "ic", sel("@class.inner"))
+        vim.keymap.set({ "x", "o" }, "a/", sel("@comment.outer"))
+        vim.keymap.set({ "x", "o" }, "i/", sel("@comment.inner"))
+        vim.keymap.set({ "x", "o" }, "ii", sel("@conditional.inner"))
+        vim.keymap.set({ "x", "o" }, "ai", sel("@conditional.outer"))
+        vim.keymap.set({ "x", "o" }, "ix", sel("@attribute.inner"))
+        vim.keymap.set({ "x", "o" }, "ax", sel("@attribute.outer"))
+
+        -- textobjects: swap
+        local swap = require("nvim-treesitter-textobjects.swap")
+        vim.keymap.set("n", "<leader>na", function() swap.swap_next("@parameter.inner") end)
+        vim.keymap.set("n", "<leader>nf", function() swap.swap_next("@function.outer") end)
+        vim.keymap.set("n", "<leader>pa", function() swap.swap_previous("@parameter.inner") end)
+        vim.keymap.set("n", "<leader>pf", function() swap.swap_previous("@function.outer") end)
+
+        -- textobjects: move
+        local move = require("nvim-treesitter-textobjects.move")
+        local mn = function(q) return function() move.goto_next_start(q, "textobjects") end end
+        local me = function(q) return function() move.goto_next_end(q, "textobjects") end end
+        local mp = function(q) return function() move.goto_previous_start(q, "textobjects") end end
+        local mpe = function(q) return function() move.goto_previous_end(q, "textobjects") end end
+
+        vim.keymap.set({ "n", "x", "o" }, "]f",  mn("@function.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "]]",  mn("@class.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "]a",  mn("@parameter.inner"))
+        vim.keymap.set({ "n", "x", "o" }, "]i",  mn("@conditional.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "]/",  mn("@comment.outer"))
+
+        vim.keymap.set({ "n", "x", "o" }, "]F",  me("@function.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "][",  me("@class.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "]A",  me("@parameter.inner"))
+        vim.keymap.set({ "n", "x", "o" }, "]I",  me("@conditional.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "]?",  me("@comment.outer"))
+
+        vim.keymap.set({ "n", "x", "o" }, "[f",  mp("@function.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "[[",  mp("@class.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "[a",  mp("@parameter.inner"))
+        vim.keymap.set({ "n", "x", "o" }, "[i",  mp("@conditional.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "[/",  mp("@comment.outer"))
+
+        vim.keymap.set({ "n", "x", "o" }, "[F",  mpe("@function.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "[]",  mpe("@class.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "[A",  mpe("@parameter.inner"))
+        vim.keymap.set({ "n", "x", "o" }, "[I",  mpe("@conditional.outer"))
+        vim.keymap.set({ "n", "x", "o" }, "[?",  mpe("@comment.outer"))
+
+        -- lsp_interop was removed upstream; use native LSP hover instead
+        vim.keymap.set("n", "<leader>df", vim.lsp.buf.hover, { desc = "LSP hover (peek definition)" })
+        vim.keymap.set("n", "<leader>dF", vim.lsp.buf.hover, { desc = "LSP hover (peek class)" })
 
         -- Repeat movement with ; and ,
-        -- ensure ; goes forward and , goes backward regardless of the last direction
-        -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-        -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
         -- vim way: ; goes to the direction you were moving.
+        local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
         vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
         vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
-        -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+        -- Make builtin f, F, t, T also repeatable with ; and ,
         vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
         vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
         vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
